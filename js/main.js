ed42914f180b4a2d25327d30dc2844d42fb2f802
Anthony Malang <anthonymalang@u.boisestate.edu>
2024-08-29 11:51:48 -0600

    Added button & functionlistener

--- js/main.js
+++ js/main.js
@@ -1,18 +1,9 @@
-window.onload = loaded;
-
-/**
- * Simple Function that will be run when the browser is finished loading.
- */
-function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
-}
-
+// window.sayHello = sayHello;
 /**
  * This function returns the string 'hello'
  * @return {string} the string hello
  */
 export function sayHello() {
-    return 'hello';
+    alert("Hello!");
 }
+document.getElementById('button1').addEventListener('click', sayHello);
